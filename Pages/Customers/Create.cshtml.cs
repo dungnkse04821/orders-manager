991fa4f567653f8041b4e289d7f82615a3e389a0
DungNK <dungnkse04821@gmail.com>
2025-12-23 23:43:22 +0700

    add create customer page

---
+++ Pages/Customers/Create.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OrdersManager.Models;
+
+namespace OrdersManager.Pages.Customers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly GoogleSheetService _service;
+        public CreateModel(GoogleSheetService service) { _service = service; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // Tự sinh ID ngắn gọn (8 ký tự)
+            Customer.Id = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+
+            _service.AddCustomer(Customer);
+            return RedirectToPage("./Index");
+        }
+    }
+}
