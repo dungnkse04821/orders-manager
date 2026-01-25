5ce9b62b93260bb6f712295fae80c2fa3a65409f
DungNK <dungnkse04821@gmail.com>
2026-01-25 20:46:50 +0700

    remove focus

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,6 +9,7 @@
         public decimal SellingPrice { get; set; }
         public string Source { get; set; }
         public string Warehouse { get; set; }
+        public string Reference { get; set; }
 
         // Property phụ để hiển thị đẹp trên Dropdown (VD: "AO01 - Áo Thun")
         public string DisplayText => $"{Sku} - {Name}";
